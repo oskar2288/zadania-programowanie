d64f211d9501294dcc676efe93a8b84ff9878b43
oskar2288 <61974322+oskar2288@users.noreply.github.com>
2021-04-27 14:26:46 +0200

    Add files via upload

---
+++ lekcja 13/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int wiek;
+    cin >> wiek;
+    if( wiek < 18 )
+   
+    cout << "Nie jestes pelnoletni" << endl;
+    else
+   
+    cout << "Jestes pelnoletni" << endl;
+    cout << "Koniec" << endl;
+    return 0;
+}
